a61fc4d6d564daafd45ff14d1e41fdb21013a8a2
nba5137 <siso8575@colorado.edu>
2018-04-30 21:57:09 -0600

    Updated Request method. Requesting for data in past 7 days.

--- website/python/tweeter_api.py
+++ website/python/tweeter_api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import datetime
 import requests
 import json
 from requests.auth import HTTPBasicAuth
@@ -8,25 +9,52 @@ from requests.auth import HTTPBasicAuth
 ## Request thru GNIP API
 hashtag1 = sys.argv[1] #raw_input("1st keyword for hashtag: \n")
 hashtag2 = sys.argv[2] #raw_input("2nd keyword for hashtag: \n")
-maxResults = sys.argv[3] #raw_input("maxResults: \n")
-fromDate = sys.argv[4] #raw_input("From the date (YYYYMMDDHHMM/201804100000) :\n")
-toDate = sys.argv[5] #raw_input("To the date (YYYYMMDDHHMM/201804150000) :\n")
+maxResults = '500'
 
-Fir_url = 'https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/prod.json?query='+hashtag1+'&maxResults='+maxResults+'&fromDate='+fromDate+'&toDate='+toDate
-Sec_url = 'https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/prod.json?query='+hashtag2+'&maxResults='+maxResults+'&fromDate='+fromDate+'&toDate='+toDate
+now = datetime.date.today()
+HH = '00'
+MM = '00'
+limit = 7
+count = 0
+f1 = open('First_key.json', 'a+')
+f2 = open('Second_key.json', 'a+')
 
-## Getting info
-request_Result1 = requests.get(Fir_url, auth=HTTPBasicAuth('yadu3240@colorado.edu', '!gty19970721'))
-request_Result2 = requests.get(Sec_url, auth=HTTPBasicAuth('yadu3240@colorado.edu', '!gty19970721'))
+for x in range(0, 7):
+    temp_date = datetime.date.today() - datetime.timedelta(days = limit)
+    limit = limit - 1;
+    if (temp_date.month < 10):
+        request_Date = str(temp_date.year) + '0' + str(temp_date.month) + str(temp_date.day)
+    else:
+        request_Date = str(temp_date.year) + str(temp_date.month) + str(temp_date.day)
+        
+    for y in range(0, 24):
+        hh = int(HH) + 1
+        if (hh < 10):
+            HH = '0' + str(hh)
+        else:
+            HH = str(hh)
+            
+        for z in range(0, 6):
+            Fir_url = ('https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/prod.json?query='+hashtag1+'&maxResults='+maxResults+'&fromDate='+request_Date+HH+MM+'&toDate='+request_Date+'2359')
+            Sec_url = ('https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/prod.json?query='+hashtag2+'&maxResults='+maxResults+'&fromDate='+request_Date+HH+MM+'&toDate='+request_Date+'2359')
+            ## Getting info
+            request_Result1 = requests.get(Fir_url, auth=HTTPBasicAuth('yadu3240@colorado.edu', '!gty19970721')).text
+            request_Result2 = requests.get(Sec_url, auth=HTTPBasicAuth('yadu3240@colorado.edu', '!gty19970721')).text
+            ## Saving as JSON
+            json.dump(request_Result1, f1)
+            json.dump(request_Result2, f2)
+            count = count + 2
+            print(count)
+            mm = int(MM) + 10
+            MM = str(mm)
+        MM = '00'      
+    HH = '00'
 
+f1.close()
+f2.close()
 ## Testing output
-print request_Result1.content
-print ('==================')
-print request_Result2.content
+#print request_Result1.content
+#print ('==================')
+#print request_Result2.content
 
-## Saving as JSON
-with open('First_key.json', 'w') as outfile:
-    	 json.dump(request_Result1.content, outfile)
 
-with open('Second_key.json', 'w') as outfile:
-    	 json.dump(request_Result2.content, outfile)
